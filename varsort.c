64e9a4afdd2a0dd2bd10426760d29386ed3b7c89
Pradeep Kashyap Ramaswamy <pradeep.kashyap@wisc.edu>
2016-09-15 13:54:49 -0500

    Checking-in the proper parsing of the input arguments

--- varsort.c
+++ varsort.c
@@ -4,14 +4,16 @@
  *	Email 	: pradeep.kashyap@wisc.edu
  *	This file sorts 'R' records based on key 'K' each with data size 'N'.
  */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "sort.h"
+#include <string.h>
 
 /* The maximum size for the file name */
 #define MAX_FNAME_SZ 50
 /* Typedefing to avoid typing full unsigned int */
-typedef unsigned int uint
+typedef unsigned int uint;
 
 typedef struct {
 	uint R;
@@ -19,10 +21,34 @@ typedef struct {
 	char *name;
 } file_data_t;
 
-int main(int arg_count, char *argv[])
+int 
+main(int arg_count, char *arg_vector[])
 {	
-	char input_filename[MAX_FNAME_SZ] = {0};
-	char output_filename[MAX_FNAME_SZ] = {0};
+	char *input_filename;
+	char *output_filename;
+	int option;
 
+	if (arg_count < 2) {	
+		fprintf(stderr, "Usage: varsort -i inputfile -o outputfile\n");
+		/* Exit with error */
+		exit(1);
+	}
+	while ((option = getopt(arg_count, arg_vector, "i:o:")) != -1) {
+		switch (option) {
+			case 'i':
+				input_filename = strdup(optarg);
+				printf("IFile = %s\n", input_filename);
+				break;
+			case 'o':
+				output_filename = strdup(optarg);
+				printf("OFile = %s\n", output_filename);
+				break;
+			default:
+				fprintf(stderr, "Usage: varsort -i inputfile -o"
+					" outputfile\n");
+				/* Exit with error */
+				exit(1);
+		}
+	}
 	exit(0);
 }
